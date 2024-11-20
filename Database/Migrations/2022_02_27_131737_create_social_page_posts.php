90fd615a13ecb91862de8ce7430ba548b1397a69
Davi Menezes <davimenezes.dev@gmail.com>
2024-11-20 16:23:21 -0300

    refactorings

--- Database/Migrations/2022_02_27_131737_create_social_page_posts.php
+++ Database/Migrations/2022_02_27_131737_create_social_page_posts.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Modules\Social\Entities\SocialPagePost\SocialPagePostEntityModel;
+use Nwidart\Modules\Facades\Module;
 
 return new class extends Migration
 {
@@ -14,7 +15,7 @@ return new class extends Migration
      */
     public function up()
     {
-        if (!collect(\Nwidart\Modules\Facades\Module::allEnabled())->contains('Post')) {
+        if (!Module::isEnabled('Post')) {
             return;
         }
 
