06f31ff812e60edcca9bed8f6aea18928e78e954
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-18 20:29:01 -0300

    Make `moduleName` method protected
    Update `moduleName` method visibility to `protected` in all `CreateMenuItemsListener` implementations and related contracts for consistency and to enforce encapsulation. This aligns visibility across multiple modules and improves code maintainability.

--- app/Listeners/CreateMenuItemsSocialListener.php
+++ app/Listeners/CreateMenuItemsSocialListener.php
@@ -6,7 +6,7 @@ use Modules\Project\Contracts\CreateMenuItemsListenerContract;
 
 class CreateMenuItemsSocialListener extends CreateMenuItemsListenerContract
 {
-    public function moduleName(): string
+    protected function moduleName(): string
     {
         return config('social.name');
     }
